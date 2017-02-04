033e3156b57710643cdafbcae78d83bb21149d9d
Nathan Walker <walkerrunpdx@gmail.com>
2017-02-04 01:17:44 -0800

    feat(iOS): wip

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -1,5 +1,6 @@
 import { Observable } from "data/observable";
 import * as app from "application";
+import { isIOS } from 'platform';
 import { LottieView } from "nativescript-lottie";
 
 declare var com: any;
@@ -13,9 +14,10 @@ export class HelloWorldModel extends Observable {
     }
 
     public lottieLoaded(args) {
+        console.log('lottieLoaded');
         try {
             this._myLottie = args.object;
-            console.log(args.object.android);
+            console.log(isIOS ? args.object.ios : args.object.android);
         } catch (error) {
             console.log(error);
         }
