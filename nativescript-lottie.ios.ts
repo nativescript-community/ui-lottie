33a4c2afe95af8a80d4d3b6b3973834490a907e6
Nathan Walker <walkerrunpdx@gmail.com>
2017-02-04 01:17:44 -0800

    feat(iOS): wip

--- nativescript-lottie.ios.ts
+++ nativescript-lottie.ios.ts
@@ -4,10 +4,58 @@
 *
 * Version 1.0.0                                           bradwaynemartin@gmail.com
 **********************************************************************************/
-'use strict';
+"use strict";
 
-export class NativescriptLottie { 
+import { View } from "ui/core/view";
+
+declare var LAAnimationView: any;
+
+export class LottieView extends View {
+  private _src: string;
+  private _ios: any;
+
   constructor() {
-    // Put in your initialization
+    super();
   }
+
+  /// LAAnimationView
+  get ios(): any {
+    return this._ios;
+  }
+  /// LAAnimationView
+  get _nativeView(): any {
+    return this._ios;
+  }
+
+  get src(): string {
+    return this._src;
+  }
+  set src(value: string) {
+    console.log('setting src:', value);
+    this._src = value;
+  }
+
+  public onLoaded() {
+    console.log('lottie onLoaded');
+    if (this._src) {
+      this._ios = LAAnimationView.animationNamed(this._src);
+      console.log(this._ios);
+      this.playAnimation();
+    }
+  }
+
+  public playAnimation(): void {
+    if (this._ios) {
+      this._ios.playWithCompletion((animationFinished: boolean) => {
+        console.log('animationFinished:', animationFinished);
+      });
+    }
+  }
+
+  public cancelAnimation(): void {
+    if (this._ios) {
+      console.log('unsupported');
+    }
+  }
+
 }
