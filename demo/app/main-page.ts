50a8f5d08e0a191fcb26c99726d9095467b9d326
rhanbIT <aniel.jeanbaptiste@gmail.com>
2017-09-27 17:25:22 +0200

    3.x.x support

--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -1,5 +1,5 @@
-import { EventData } from "data/observable";
-import { Page } from "ui/page";
+import { EventData } from "tns-core-modules/data/observable";
+import { Page } from "tns-core-modules/ui/page";
 import { HelloWorldModel } from "./main-view-model";
 
 export function navigatingTo(args: EventData) {
