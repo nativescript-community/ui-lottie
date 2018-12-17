ef9cbf6584e55f01e0b0a1bb245d388f0932fcda
Brad Martin <bradwaynemartin@gmail.com>
2018-12-17 11:23:05 -0600

    update ng-demo

---
+++ demo-ng/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import {
+  NgModule,
+  NgModuleFactoryLoader,
+  NO_ERRORS_SCHEMA
+} from '@angular/core';
+import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  bootstrap: [AppComponent],
+  imports: [NativeScriptModule, AppRoutingModule],
+  declarations: [AppComponent, HomeComponent],
+  schemas: [NO_ERRORS_SCHEMA]
+})
+export class AppModule {}
