edd2365e85052664c1992247c238331df0f96422
Brad Martin <bradwaynemartin@gmail.com>
2019-03-01 16:52:14 -0600

    vue demo

---
+++ demo-vue/app/components/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <Page class="page">
+    <ActionBar class="action-bar">
+      <Label class="action-bar-title" text="Home"></Label>
+    </ActionBar>
+
+    <StackLayout>
+      <Label class="info" horizontalAlignment="center" verticalAlignment="center">
+        <FormattedString>
+          <Span class="fa" text.decode="&#xf135; "/>
+          <Span :text="message"/>
+        </FormattedString>
+      </Label>
+      <LottieView src="AndroidWave.json" height="130" loop="true" autoplay="true"></LottieView>
+    </StackLayout>
+  </Page>
+</template>
+
+<script>
+export default {
+  computed: {
+    message() {
+      return 'Blank {N}-Vue app';
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+// Start custom common variables
+@import '../app-variables';
+// End custom common variables
+
+// Custom styles
+.fa {
+  color: $accent-dark;
+}
+
+.info {
+  font-size: 20;
+}
+</style>
