fed50251d67f217900b796882edb34eb6d509dcf
farfromrefuge <dev@akylas.fr>
2023-11-20 16:30:00 +0100

    feat: keyPathColors property (`setColor` as a property)

--- src/lottie.common.ts
+++ src/lottie.common.ts
@@ -5,7 +5,11 @@
  * Version 1.0.0                                           walkerrunpdx@gmail.com
  **********************************************************************************/
 
-import { CoreTypes, Property, View, booleanConverter } from '@nativescript/core';
+import { Color, CoreTypes, Property, View, booleanConverter } from '@nativescript/core';
+
+export interface KeyPathColors {
+    [k: string]: Color | string;
+}
 
 export class LottieViewBase extends View {
     public stretch: CoreTypes.ImageStretchType;
@@ -14,7 +18,13 @@ export class LottieViewBase extends View {
     public loop: boolean;
     public autoPlay: boolean;
     public progress: number;
+    public keyPathColors: KeyPathColors;
     public completionBlock: (animationFinished: boolean) => void;
+    // public abstract setColor(value: Color, keyPath: string[]): void;
+
+    // [keyPathColorsProperty.setNative](value) {
+    //     Object.keys(value).forEach((k) => this.setColor(value[k], k.split('|')));
+    // }
 }
 
 export const srcProperty = new Property<LottieViewBase, string>({
@@ -58,3 +68,8 @@ export const stretchProperty = new Property<LottieViewBase, CoreTypes.ImageStret
     affectsLayout: global.isIOS
 });
 stretchProperty.register(LottieViewBase);
+
+export const keyPathColorsProperty = new Property<LottieViewBase, KeyPathColors>({
+    name: 'keyPathColors'
+});
+keyPathColorsProperty.register(LottieViewBase);
